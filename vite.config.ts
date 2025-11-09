aacffc21891cf818160e4d160fd26757bb9f9f64
TechLEarners2000 <techl.earners.offl@gmail.com>
2025-11-09 07:25:55 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    allowedHosts: mode === 'development' ? ['.onrender.com'] : undefined,
+     allowedHosts: ["humanet-1.onrender.com"]
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
